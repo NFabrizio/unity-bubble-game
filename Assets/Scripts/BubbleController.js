5a7868add77aab2badba5cd8720c651066b4c536
NFabrizio <nick.fabrizio@pearson.com>
2017-09-08 09:12:56 -0600

    Adds script to control bubble click and pop

---
+++ Assets/Scripts/BubbleController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+private var anim : Animator;
+function Start () {
+	anim = GetComponent.<Animator>();
+}
+
+function Update () {
+	
+}
+
+function OnMouseDown () {
+  Debug.Log("You clicked the bubble");
+  anim.SetTrigger("Popping");
+  Destroy(gameObject, 0.5);
+}
