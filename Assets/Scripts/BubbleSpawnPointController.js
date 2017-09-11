2232345884ab8c2be16064f77717c307a0474a16
NFabrizio <nick.fabrizio@pearson.com>
2017-09-11 08:33:04 -0600

    Adds instantiation of more bubbles and random time generation of new bubbles

---
+++ Assets/Scripts/BubbleSpawnPointController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var BubblePrefab : GameObject;
+private var nextGenTime : float;
+var DelayUpper : float;
+var DelayLower : float;
+
+function Start () {
+	
+}
+
+function Update () {
+  if(Time.time > nextGenTime) {
+	Instantiate(BubblePrefab, transform.position, Quaternion.identity);
+	var randomDelay : float = Random.Range(DelayLower, DelayUpper);
+	nextGenTime = Time.time + randomDelay;
+  }
+}
