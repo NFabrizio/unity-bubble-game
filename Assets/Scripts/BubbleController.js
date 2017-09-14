30bdd167779000de89e4f8f8ec9812f8c3e0732b
NFabrizio <nick.fabrizio@pearson.com>
2017-09-14 08:55:49 -0600

    Adds score keeping and points for popping bubbles

--- Assets/Scripts/BubbleController.js
+++ Assets/Scripts/BubbleController.js
@@ -7,6 +7,7 @@ private var rb : Rigidbody2D;
 // Public vars will show in the Unity editor component
 public var thrust : float = 5;
 public var destroyDelay : float = 15.0f;
+public var PointValue : int = 100;
 
 function Start () {
 	anim = GetComponent.<Animator>();
@@ -24,6 +25,10 @@ function Update () {
 function OnMouseDown () {
 //  Debug.Log("You clicked the bubble");
 
+  var gc : GameObject = GameObject.FindGameObjectWithTag("GameController");
+  var scoreKeeper : ScoreKeeper = gc.GetComponent.<ScoreKeeper>();
+
+  scoreKeeper.AddPoints(PointValue);
   audioSrc.Play();
   anim.SetTrigger("Popping");
   Destroy(gameObject, 0.5);
