ed9d03b33b281eaa4af8cbd549cb74e678c673c9
NFabrizio <nick.fabrizio@pearson.com>
2017-09-22 08:51:00 -0600

    Adds call to subtract lives if bubble crosses top boundary

--- Assets/Scripts/DestroyByBoundary.js
+++ Assets/Scripts/DestroyByBoundary.js
@@ -1,20 +1,18 @@
 ﻿#pragma strict
 
+private var gameController : GameController;
+private var gc : GameObject;
 private var stageDimensions : Vector3;
 
 function Start() {
+	gc = GameObject.FindGameObjectWithTag("GameController");
+	gameController = gc.GetComponent.<GameController>();
     stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
-Debug.Log("stageDimensions");
-Debug.Log(stageDimensions.x);
-Debug.Log(stageDimensions.y);
     var newPosition : Vector3 = new Vector3(stageDimensions.x, stageDimensions.y, stageDimensions.z);
-Debug.Log("transform.position1");
-Debug.Log(transform.position);
 	transform.position = newPosition;
-Debug.Log("transform.position2");
-Debug.Log(transform.position);
 }
 
 function OnTriggerExit2D(other: Collider2D) {
+    gameController.SubtractLives();
     Destroy(other.gameObject);
 }
