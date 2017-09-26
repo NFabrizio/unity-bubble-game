c0c59e7f039414e34d43831ed2430b1e818464f0
NFabrizio <nick.fabrizio@pearson.com>
2017-09-26 08:55:21 -0600

    Adds game restart functionality and button

--- Assets/Scripts/GameController.js
+++ Assets/Scripts/GameController.js
@@ -1,16 +1,22 @@
 ﻿#pragma strict
 
+import UnityEngine.SceneManagement;
+
 private var gameOver : boolean;
+private var gameRestart : boolean;
 
 public var Score : int = 0;
 public var ScoreDisplay : UI.Text;
 public var Lives : int = 3;
 public var LifeDisplay : UI.Text;
 public var GameOverDisplay : UI.Text;
+public var GameRestartButton : UI.Button;
 
 function Start () {
 	gameOver = false;
+	gameRestart = false;
 	GameOverDisplay.text = "";
+	GameRestartButton.gameObject.SetActive(false);
 }
 
 function Update () {
@@ -22,19 +28,25 @@ public function AddPoints(howMany : int) {
     Score += howMany;
 }
 
+public function GameOver() {
+	gameOver = true;
+	gameRestart = true;
+    GameOverDisplay.text = "Game Over";
+    GameRestartButton.gameObject.SetActive(true);
+}
+
+public function GameRestart() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+public function GetGameStatus() {
+	return gameOver;
+}
+
 public function SubtractLives() {
     Lives -= 1;
 
     if(Lives == 0) {
 	    GameOver();
     }
-}
-
-public function GameOver() {
-	gameOver = true;
-    GameOverDisplay.text = "Game Over";
-}
-
-public function GetGameStatus() {
-	return gameOver;
 }
