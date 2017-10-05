78f9f1669e6f5a69de262a96a25be58e584fc242
NFabrizio <nick.fabrizio@pearson.com>
2017-10-05 08:21:11 -0600

    Moves destroy boundary up a little so that bubbles do not get destroyed until completely off screen

--- Assets/Scripts/DestroyByBoundary.js
+++ Assets/Scripts/DestroyByBoundary.js
@@ -2,13 +2,14 @@
 
 private var gameController : GameController;
 private var gc : GameObject;
+private var newPosition : Vector3;
 private var stageDimensions : Vector3;
 
 function Start() {
 	gc = GameObject.FindGameObjectWithTag("GameController");
 	gameController = gc.GetComponent.<GameController>();
     stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-    var newPosition : Vector3 = new Vector3(stageDimensions.x, stageDimensions.y, stageDimensions.z);
+    newPosition = new Vector3(stageDimensions.x, stageDimensions.y + 0.5, stageDimensions.z);
 	transform.position = newPosition;
 }
 
